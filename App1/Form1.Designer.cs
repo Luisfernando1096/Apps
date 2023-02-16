a84326f09f6a06251ebfc7287a0bec5608a32cba
fruiz <fruiz@LAPTOP-QBB4AB87>
2023-02-16 10:56:22 -0600

    boton movido

--- App1/Form1.Designer.cs
+++ App1/Form1.Designer.cs
@@ -37,7 +37,7 @@ namespace App1
             // button1
             // 
             this.button1.Location = new System.Drawing.Point(52, 91);
-            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(56, 19);
             this.button1.TabIndex = 0;
@@ -48,7 +48,7 @@ namespace App1
             // 
             this.checkedListBox1.FormattingEnabled = true;
             this.checkedListBox1.Location = new System.Drawing.Point(61, 153);
-            this.checkedListBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.checkedListBox1.Margin = new System.Windows.Forms.Padding(2);
             this.checkedListBox1.Name = "checkedListBox1";
             this.checkedListBox1.Size = new System.Drawing.Size(91, 64);
             this.checkedListBox1.TabIndex = 1;
