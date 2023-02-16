c29894838b612a90f9dde7a7931784b930b50eea
fruiz <fruiz@LAPTOP-QBB4AB87>
2023-02-16 11:15:15 -0600

    Colocado mejor el boton

--- App1/Form1.Designer.cs
+++ App1/Form1.Designer.cs
@@ -36,10 +36,10 @@ namespace App1
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(52, 91);
+            this.button1.Location = new System.Drawing.Point(39, 74);
             this.button1.Margin = new System.Windows.Forms.Padding(2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(56, 19);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
@@ -47,31 +47,29 @@ namespace App1
             // checkedListBox1
             // 
             this.checkedListBox1.FormattingEnabled = true;
-            this.checkedListBox1.Location = new System.Drawing.Point(61, 153);
-            this.checkedListBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.checkedListBox1.Location = new System.Drawing.Point(46, 124);
+            this.checkedListBox1.Margin = new System.Windows.Forms.Padding(2);
             this.checkedListBox1.Name = "checkedListBox1";
-            this.checkedListBox1.Size = new System.Drawing.Size(120, 72);
+            this.checkedListBox1.Size = new System.Drawing.Size(91, 49);
             this.checkedListBox1.TabIndex = 1;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(69, 168);
-            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Location = new System.Drawing.Point(276, 124);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(147, 89);
+            this.button2.Size = new System.Drawing.Size(110, 72);
             this.button2.TabIndex = 2;
             this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(583, 303);
+            this.ClientSize = new System.Drawing.Size(437, 246);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.checkedListBox1);
             this.Controls.Add(this.button1);
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
