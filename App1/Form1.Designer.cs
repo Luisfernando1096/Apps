80b7b6c7415e573ffe2c6960baf76ae672a356f5
EdgarFuentes100 <edgarfuentes139@gmail.com>
2023-03-12 18:12:13 -0500

    Cambio

--- App1/Form1.Designer.cs
+++ App1/Form1.Designer.cs
@@ -37,14 +37,16 @@ namespace App1
             this.Hombre = new System.Windows.Forms.CheckBox();
             this.label1 = new System.Windows.Forms.Label();
             this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.button4 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(39, 74);
-            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Location = new System.Drawing.Point(52, 91);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(56, 19);
+            this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
@@ -52,17 +54,18 @@ namespace App1
             // checkedListBox1
             // 
             this.checkedListBox1.FormattingEnabled = true;
-            this.checkedListBox1.Location = new System.Drawing.Point(46, 124);
-            this.checkedListBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.checkedListBox1.Location = new System.Drawing.Point(61, 153);
+            this.checkedListBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.checkedListBox1.Name = "checkedListBox1";
-            this.checkedListBox1.Size = new System.Drawing.Size(91, 49);
+            this.checkedListBox1.Size = new System.Drawing.Size(120, 55);
             this.checkedListBox1.TabIndex = 1;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(276, 124);
+            this.button2.Location = new System.Drawing.Point(368, 153);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(110, 72);
+            this.button2.Size = new System.Drawing.Size(147, 89);
             this.button2.TabIndex = 2;
             this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = true;
@@ -70,16 +73,18 @@ namespace App1
             // checkedListBox2
             // 
             this.checkedListBox2.FormattingEnabled = true;
-            this.checkedListBox2.Location = new System.Drawing.Point(159, 24);
+            this.checkedListBox2.Location = new System.Drawing.Point(212, 30);
+            this.checkedListBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.checkedListBox2.Name = "checkedListBox2";
-            this.checkedListBox2.Size = new System.Drawing.Size(120, 64);
+            this.checkedListBox2.Size = new System.Drawing.Size(159, 72);
             this.checkedListBox2.TabIndex = 3;
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(329, 48);
+            this.button3.Location = new System.Drawing.Point(439, 59);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.Size = new System.Drawing.Size(100, 28);
             this.button3.TabIndex = 4;
             this.button3.Text = "button3";
             this.button3.UseVisualStyleBackColor = true;
@@ -87,9 +92,10 @@ namespace App1
             // Hombre
             // 
             this.Hombre.AutoSize = true;
-            this.Hombre.Location = new System.Drawing.Point(32, 229);
+            this.Hombre.Location = new System.Drawing.Point(43, 282);
+            this.Hombre.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Hombre.Name = "Hombre";
-            this.Hombre.Size = new System.Drawing.Size(63, 17);
+            this.Hombre.Size = new System.Drawing.Size(80, 21);
             this.Hombre.TabIndex = 3;
             this.Hombre.Text = "Hombre";
             this.Hombre.UseVisualStyleBackColor = true;
@@ -98,36 +104,48 @@ namespace App1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(94, 202);
+            this.label1.Location = new System.Drawing.Point(125, 249);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(74, 24);
+            this.label1.Size = new System.Drawing.Size(94, 29);
             this.label1.TabIndex = 4;
             this.label1.Text = "Genero";
             // 
             // checkBox1
             // 
             this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(160, 229);
+            this.checkBox1.Location = new System.Drawing.Point(213, 282);
+            this.checkBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(52, 17);
+            this.checkBox1.Size = new System.Drawing.Size(65, 21);
             this.checkBox1.TabIndex = 5;
             this.checkBox1.Text = "Mujer";
             this.checkBox1.UseVisualStyleBackColor = true;
             // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(61, 44);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(437, 246);
+            this.ClientSize = new System.Drawing.Size(583, 352);
+            this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.checkedListBox2);
-            this.ClientSize = new System.Drawing.Size(437, 286);
             this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.Hombre);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.checkedListBox1);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -145,6 +163,8 @@ namespace App1
         private System.Windows.Forms.CheckBox Hombre;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.CheckBox checkBox1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button button4;
     }
 }
 
